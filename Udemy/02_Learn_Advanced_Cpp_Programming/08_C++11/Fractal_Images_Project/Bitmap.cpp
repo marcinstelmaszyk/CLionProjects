747a229920350612d21284e259bc5b470d3aceef
Marcin Stelmaszyk <stelmaszyk.m@gmail.com>
2020-05-01 20:46:53 +0200

    Populate Bitmap::write()

--- Udemy/02_Learn_Advanced_Cpp_Programming/08_C++11/Fractal_Images_Project/Bitmap.cpp
+++ Udemy/02_Learn_Advanced_Cpp_Programming/08_C++11/Fractal_Images_Project/Bitmap.cpp
@@ -1,12 +1,29 @@
 #include "Bitmap.h"
+#include "BitmapInfoHeader.h"
+#include "BitmapFileHeader.h"
 
 namespace btmap {
 
     Bitmap::Bitmap(int width, int height)
             : width(width), height(height),
-              pPixels(new uint8_t[width*height*3]) {}
+              pPixels(new uint8_t[width*height*3]{}) {}
 
     bool Bitmap::write(std::string filename) {
+        BitmapFileHeader fileHeader;
+        BitmapInfoHeader infoHeader;
+
+        // Calculate the size of the file
+        fileHeader.fileSize = sizeof(BitmapFileHeader)
+                + sizeof(BitmapInfoHeader)
+                + width * height * 3;
+
+        // Set the data offset
+        fileHeader.dataOffset = sizeof(BitmapFileHeader)
+                + sizeof(BitmapInfoHeader);
+
+        infoHeader.width = width;
+        infoHeader.height = height;
+
         return false;
     }
 
