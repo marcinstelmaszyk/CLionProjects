ac03b1fd9e16f73b15bb519a4eabcd2aaa4f5143
Marcin Stelmaszyk <stelmaszyk.m@gmail.com>
2020-05-02 14:04:10 +0200

    First Mandelbrot image

--- Udemy/02_Learn_Advanced_Cpp_Programming/08_C++11/Fractal_Images_Project/Mandelbrot.cpp
+++ Udemy/02_Learn_Advanced_Cpp_Programming/08_C++11/Fractal_Images_Project/Mandelbrot.cpp
@@ -1,3 +1,4 @@
+#include <complex>
 #include "Mandelbrot.h"
 
 namespace btmap {
@@ -5,7 +6,21 @@ namespace btmap {
     Mandelbrot::Mandelbrot() {}
 
     int btmap::Mandelbrot::getIterations(double x, double y) {
-        return 0;
+        std::complex<double> z = 0;
+        std::complex<double> c(x, y);
+
+        int iterations = 0;
+
+        while (iterations < MAX_ITERATIONS) {
+            z = z*z + c;
+
+            if (std::norm(z) > 2)
+                break;
+
+            ++iterations;
+        }
+
+        return iterations;
     }
 
 }
--- Udemy/02_Learn_Advanced_Cpp_Programming/08_C++11/Fractal_Images_Project/main.cpp
+++ Udemy/02_Learn_Advanced_Cpp_Programming/08_C++11/Fractal_Images_Project/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Bitmap.h"
+#include "Mandelbrot.h"
 
 using namespace std;
 using namespace btmap;
@@ -10,10 +11,25 @@ int main() {
 
     Bitmap bitmap(WIDTH, HEIGHT);
 
-    for (int y = 0; y < HEIGHT/3; y++) {
-        for (int x = 0; x < WIDTH/2; x++)
-            bitmap.setPixel(x, y, 0, 255, 0);
+    double min = 999999;
+    double max = -999999;
+
+    for (int y = 0; y < HEIGHT; y++) {
+        for (int x = 0; x < WIDTH; x++){
+            double xFractal = (x - WIDTH/2) * 2.0/WIDTH;
+            double yFractal = (y - HEIGHT/2) * 2.0/HEIGHT;
+
+            int iterations = Mandelbrot::getIterations(xFractal, yFractal);
+
+            uint8_t red = (uint8_t)(256 * static_cast<double>(iterations) / Mandelbrot::MAX_ITERATIONS);
+
+            bitmap.setPixel(x, y, red, red,red);
+            if(red < min) min = red;
+            if(red > max) max = red;
+        }
     }
+
+    std::cout << min << ", " << max << std::endl;
 
     bitmap.write("test.bmp");
 
