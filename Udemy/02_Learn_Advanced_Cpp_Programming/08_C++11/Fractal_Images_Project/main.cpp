2947a313f0689e9e1b1042717a595c417d238a42
Marcin Stelmaszyk <stelmaszyk.m@gmail.com>
2020-05-02 19:46:22 +0200

    Improved coloring

--- Udemy/02_Learn_Advanced_Cpp_Programming/08_C++11/Fractal_Images_Project/main.cpp
+++ Udemy/02_Learn_Advanced_Cpp_Programming/08_C++11/Fractal_Images_Project/main.cpp
@@ -16,16 +16,18 @@ int main() {
 
     for (int y = 0; y < HEIGHT; y++) {
         for (int x = 0; x < WIDTH; x++){
-            double xFractal = (x - WIDTH/2) * 2.0/WIDTH;
+            double xFractal = (x - WIDTH/2) * 2.0/HEIGHT;
             double yFractal = (y - HEIGHT/2) * 2.0/HEIGHT;
 
             int iterations = Mandelbrot::getIterations(xFractal, yFractal);
 
-            uint8_t red = (uint8_t)(256 * static_cast<double>(iterations) / Mandelbrot::MAX_ITERATIONS);
+            uint8_t color = (uint8_t)(256 * static_cast<double>(iterations) / Mandelbrot::MAX_ITERATIONS);
 
-            bitmap.setPixel(x, y, red, red,red);
-            if(red < min) min = red;
-            if(red > max) max = red;
+            color = color * color * color;
+
+            bitmap.setPixel(x, y, 0, color, 0);
+            if(color < min) min = color;
+            if(color > max) max = color;
         }
     }
 
